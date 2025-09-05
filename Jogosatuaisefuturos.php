ffa10b9fd3f39481b7ad856a9a823db2e9824782
Joaoterminus3110 <joaomateusalcantara2003@gmail.com>
2025-09-04 21:56:49 -0300

    pagian futuro jogo feita

--- Jogosatuaisefuturos.php
+++ Jogosatuaisefuturos.php
@@ -5,11 +5,11 @@ session_start();
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8" >
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
   <title>KILL THE SUN</title>
-  <link rel="icon" href="Imagem/icono_assustado.png" />
-  <link rel="stylesheet" href="css/style.css" />
+  <link rel="icon" href="Imagem/icono_assustado.png" >
+  <link rel="stylesheet" href="css/style.css" >
 
 </head>
 <body>
@@ -33,17 +33,18 @@ session_start();
 
   <main>
     <main>
-    <div class="Jogo1">
-        <h2>Kill the Sun</h2>
-          <img src="Imagem/Capadojogo1.png"  class="CAPA" alt="Capa do jogo Kill the Sun" style="width:300px; height:300px;">
-            <p>Kill the Sun é um jogo de plataforma 2D desenvolvido por João Mateus Alcantara Dos Santos, inspirado no livro "Kill the Sun" de J. M. Alcantara. O jogo apresenta uma jogabilidade desafiadora, onde os jogadores controlam um personagem que deve superar obstáculos e inimigos para avançar pelas fases. Com gráficos pixelados e uma trilha sonora envolvente, Kill the Sun oferece uma experiência nostálgica para os fãs de jogos de plataforma clássicos. O enredo do jogo gira em torno da luta do protagonista contra forças sombrias que ameaçam o mundo, proporcionando uma narrativa cativante que complementa a ação intensa do gameplay.</p>
+    <div class="Jogo1" >
+          <img src="Imagem/Capadojogo1.png"  class="Capa" alt="Capa do jogo Kill the Sun" style="display: block; width: 300px; height: 300px;
+  margin: 0 auto;">
+            <p>Kill the Sun é um jogo de plataforma 2D desenvolvido por João Mateus Alcantara Dos Santos, inspirado no livro "Warmaisach" de . O jogo apresenta uma jogabilidade desafiadora, onde os jogadores controlam um personagem que deve superar obstáculos e inimigos para avançar pelas fases. Com gráficos pixelados . Kill the Sun oferece uma experiência nostálgica para os fãs de jogos de plataforma clássicos. O enredo do jogo gira em torno da luta do protagonista contra forças sombrias que ameaçam o mundo, proporcionando uma narrativa cativante que complementa a ação intensa do gameplay.</p>
         </div>
     </div>
     <div class="Jogo2">
-        <h2 style="color:white" >Kill the Sun 2</h2>
-        <div class="galeria-container">
-          <img src="Imagem/FAE21.png" alt="Cena fase21" class="fase21">
-
+          <img src="Imagem/Capajogo2.png"  class="Capa" alt="Capa do jogo Kill the Sun" style="display: block; width: 300px; height: 300px;
+  margin: 0 auto;">
+  <p>Futuro Jogo Supremacy Games: Em desenvolvimento</p>
+        </div>
+   
   <footer>
     <img src="Imagem/soldorock2.png" alt="Solpagina" class="SIRIUS" />
     <p>Desenvolvido por João Mateus Alcantara Dos Santos </p>
